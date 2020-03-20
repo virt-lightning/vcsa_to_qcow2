ac5e7da27e5a751d84c1785d1d078161566dea6d
Gonéri Le Bouder <goneri@lebouder.net>
2020-03-20 17:43:53 -0400

    update for vcenter7

--- run.sh
+++ run.sh
@@ -10,16 +10,21 @@ fi
 VERSION=$(basename -s .iso ${iso_path})
 vl up
 vl ansible_inventory>inventory
-ansible-playbook install_vcsa.yml -i inventory -e vcenter_instance.installation.iso_path=${iso_path}
 
-echo 'vcenter ansible_host=192.168.123.90 ansible_user=root ansible_password="!234AaAa56" ansible_python_interpreter=/usr/bin/python ansible_ssh_common_args="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"' > inventory
+extra_args="vcenter_instance_installation_iso_path=${iso_path}"
+ansible-playbook install_vcsa.yml -i inventory -e "${extra_args}" -vvv
 
-ansible-playbook prepare_vm.yml -i inventory
+echo 'vcenter ansible_host=192.168.123.90 ansible_user=root ansible_password="!234AaAa56" ansible_python_interpreter=/usr/bin/python ansible_ssh_common_args="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes"' > inventory
+
+ansible-playbook prepare_vm.yml -i inventory -vvv
 ansible-playbook zuul_user.yml -i inventory
 ansible-playbook add_local_ssh_pubkey.yml -i inventory
 
 # If we don't reboot the ESXi, the download fails with an err 500, probably because the VM disks are still used
 ansible-playbook shutdown_esxi.yml -i inventory
+
+sleep 300
+
 ssh root@192.168.123.5 sh -c 'reboot&'
 sleep 300
 curl -v -k --user 'root:!234AaAa56' -o vCenterServerAppliance.raw 'https://192.168.123.5/folder/vCenter-Server-Appliance/vCenter-Server-Appliance-flat.vmdk?dcPath=ha%252ddatacenter&dsName=local'
